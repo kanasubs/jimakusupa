602659b765f9c28bf81fc1d39f72e4fb40b581f3
Tigr <tigr42@centrum.cz>
2013-01-01 08:49:50 +0100

    Remove lein-cljsbuild for now

--- project.clj
+++ project.clj
@@ -2,16 +2,7 @@
   :description "A Clojure library for working with subtitles"
   :url "https://github.com/tigr42/clojuresubs"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+            :url "http://www.eclipse.org/legal/epl-v10.html"
+            :distribution :repo}
   :dependencies [[org.clojure/clojure "1.4.0"]]
-  :plugins [[lein-cljsbuild "0.2.10"]]
-  :hooks [leiningen.cljsbuild]
-  :cljsbuild {:builds [{:source-path "src-cljs"
-                        :jar true
-                        :compiler {:output-to "javascript/clojuresubs.js"
-                                   :optimizations :none
-                                   :pretty-print true}}]
-              :crossovers [clojuresubs.core
-                           clojuresubs.utils]
-              :crossover-path "src-cljs"
-              :crossover-jar false})
+  :plugins [[lein-marginalia "0.7.1"]])
